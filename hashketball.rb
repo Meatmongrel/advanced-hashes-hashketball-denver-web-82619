b5b7b1933a40980d74087253011829b1315e1b41
Andrew Thiessen <squaids449@gmail.com>
2019-08-26 21:50:33 +0000

    Automatically backed up by Learn

--- hashketball.rb
+++ hashketball.rb
@@ -117,12 +117,9 @@ end
 def num_points_scored(players)
   game_hash.each do |place, team|
     team.each do |att, data|
-      binding.pry
-      if att == :players
-        data.each do |player|
-          if player[:player_name] == players
-            return player[:points]
-          end
+      data.each do |player|
+        if att == :players && player[:player_name] == players
+          return player[:points]
         end
       end
     end
