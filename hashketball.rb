a3acdec7ee07918c659880fdd67680958096a7c3
Andrew Thiessen <squaids449@gmail.com>
2019-08-26 21:37:48 +0000

    Automatically backed up by Learn

--- hashketball.rb
+++ hashketball.rb
@@ -132,8 +132,8 @@ end
 def shoe_size(players)
     game_hash.each do |place, team|
     team.each do |att, data|
-      # if att == :players
       binding.pry
+      if att == :players
         data.each do |player|
           if player[:player_name] == players
             return player[:shoe]
